23cb652b118c58d64715359d461060efa9cd0a21
Bojan Komazec <bojan.komazec@gmail.com>
2018-07-11 14:16:18 +0100

    Improved source code files organization. Added scaled image and option to select original or scaled image. All paths are local and hardcoded.

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+console.log('main.js');
+
+(function(){
+    let imageList = document.getElementById("image-list");
+    let image = document.getElementById("image");
+
+    imageList.onchange = function(evt) {
+        let selectedText = imageList.options[imageList.selectedIndex].text;
+        let imageName = imageList.options[imageList.selectedIndex].value;
+        console.log(`Selected text: ${selectedText}. Image name: ${imageName}.`);
+        let selectedImagePath = "/images/" + imageName;
+
+        image.setAttribute("src", selectedImagePath);
+        image.style.display = 'none';
+        image.style.display = 'block';
+    };
+
+    let buttonStart = document.getElementById("start-extraction");
+    buttonStart.onclick = function(evt) {
+        Tesseract.recognize(image)
+        .progress(function(p) {
+            console.log('progress', p);
+            document.getElementById("status").innerHTML = p.status;
+            document.getElementById("progress").innerHTML = p.progress;
+        })
+        .then(function(result){
+            console.log(result);
+            document.getElementById("result").innerHTML = result.text;
+        });
+    };
+})();
