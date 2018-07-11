1ed133a34c070af34d2508ec6b35bfcda31579d0
Bojan Komazec <bojan.komazec@avast.com>
2018-07-11 12:36:11 +0100

    Added tracking the status and the progress of the process.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 console.log('main.js');
 
-let myImage = document.getElementById("image");
-Tesseract.recognize(myImage)
-.then(function(result){
-    console.log(result);
-    document.getElementById("result").innerHTML = result.text;
-})
+(function(){
+    let myImage = document.getElementById("image");
+    Tesseract.recognize(myImage)
+    .progress(function(p) {
+        console.log('progress', p);
+        document.getElementById("status").innerHTML = p.status;
+        document.getElementById("progress").innerHTML = p.progress;
+    })
+    .then(function(result){
+        console.log(result);
+        document.getElementById("result").innerHTML = result.text;
+    });
+})();
